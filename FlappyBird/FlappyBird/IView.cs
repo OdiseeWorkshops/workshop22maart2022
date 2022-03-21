e017fb46c6b603ac8cd49d3da0b28cfc74382cfe
jensbaetens-odisee <83013151+jensbaetens-odisee@users.noreply.github.com>
2022-03-21 10:50:27 +0100

    Add files via upload

---
+++ FlappyBird/FlappyBird/IView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyBird
+{
+    interface IView
+    {
+        void SetObstacle(string name, double x_center, double y_center, double vertical_gap);
+        void SetObject(string name, double x_center, double y_center);
+        void SetObject(string name, double x_center, double y_center, double rotation);
+        void SetScore(string name, string text);
+    }
+}
